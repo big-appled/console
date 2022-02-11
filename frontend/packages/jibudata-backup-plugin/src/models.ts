13a4b95d3b114f3163e2759b6c2673ebba7799a3
big-appled <87792656+big-appled@users.noreply.github.com>
2022-02-11 14:03:01 +0800

    add backupjob list

--- frontend/packages/jibudata-backup-plugin/src/models.ts
+++ frontend/packages/jibudata-backup-plugin/src/models.ts
@@ -11,3 +11,27 @@ export const BackupModel: K8sKind = {
   crd: true,
   plural: 'backups',
 };
+
+export const BackupJobModel: K8sKind = {
+    kind: 'BackupJob',
+    label: 'BackupJob',
+    labelPlural: 'backupjobs',
+    apiGroup: 'migration.yinhestor.com',
+    apiVersion: 'v1alpha1',
+    abbr: 'BKPJ',
+    namespaced: true,
+    crd: true,
+    plural: 'backupjobs',
+};
+
+export const VeleroBackupModel: K8sKind = {
+    kind: 'Backup',
+    label: 'Backup',
+    labelPlural: 'backups',
+    apiGroup: 'velero.io',
+    apiVersion: 'v1',
+    abbr: 'VBKP',
+    namespaced: true,
+    crd: true,
+    plural: 'backups',
+};
