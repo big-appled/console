9a4761f04d4a083d0c17ded0cfd34cce531c1af8
Ning Ding <834652870@qq.com>
2023-02-19 10:19:13 +0800

    Update details.tsx

--- frontend/public/components/factory/details.tsx
+++ frontend/public/components/factory/details.tsx
@@ -81,7 +81,7 @@ export const DetailsPage = withFallback<DetailsPageProps>(({ pages = [], ...prop
             );
           }
           return (
-            p.properties.model.group === kindObj.apiGroup &&
+            p.properties.model.group === kindObj?.apiGroup &&
             p.properties.model.kind === kindObj.kind
           );
         })
